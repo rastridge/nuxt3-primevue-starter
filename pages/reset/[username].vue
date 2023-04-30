7b54a853ef306a6666cb8b80baf701fc789b8039
rastridge <rfa@me.com>
2023-04-30 10:55:45 -0400

    working on reset password

---
+++ pages/reset/[username].vue
@@ -0,0 +1,56 @@
+<template>
+	<div>
+		username {{ username }}
+		<FormKit
+			type="form"
+			id="password_reset"
+			submit-label="Submit"
+			@submit="submitHandler"
+		>
+			<FormKit
+				type="password"
+				name="password"
+				label="Password"
+				v-model="password"
+				validation="required|length:8"
+				validation-visibility="live"
+				:validation-messages="{
+					matches: 'Must be at leat 8 characters',
+				}"
+			/>
+			<FormKit
+				type="password"
+				name="password_confirm"
+				label="Confirm password"
+				v-model="password_confirm"
+				validation="required|confirm"
+				validation-visibility="live"
+				validation-label="Confirmation"
+			/>
+			<pre wrap>password= {{ password }}</pre>
+			<pre wrap>username= {{ username }}</pre>
+		</FormKit>
+	</div>
+</template>
+
+<script setup>
+	import '@formkit/themes/genesis'
+
+	const route = useRoute()
+	const password = ref('')
+	const password_confirm = ref('')
+
+	const username = ref(route.params.username)
+
+	const submitHandler = async () => {
+		const { data, error } = await useFetch('/api/users/resetpassword', {
+			method: 'POST',
+			body: { username, password },
+			headers: {
+				authorization: 'not-needed',
+			},
+		})
+
+		navigateTo('/loginpage')
+	}
+</script>
