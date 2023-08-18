e502090693feb520fe71f447effff66c98215657
rastridge <rfa@me.com>
2023-08-18 10:23:10 -0400

    Improve ExposeEmail visibility

--- components/ExposeEmail.vue
+++ components/ExposeEmail.vue
@@ -1,9 +1,13 @@
 <template>
 	<div>
-		<span v-if="!hover" style="color: #00c" @mouseover="hover = true"
+		<!-- <span v-if="!hover" style="color: #00c" @mouseover="hover = true"
 			>Email</span
 		>
 		<span v-if="hover" style="color: black" @mouseleave="hover = false">
+			<a :href="'mailto:' + email">{{ email }}</a>
+		</span> -->
+		<span v-if="!hover" @mouseover="hover = true">Email</span>
+		<span v-if="hover" @mouseleave="hover = false">
 			<a :href="'mailto:' + email">{{ email }}</a>
 		</span>
 	</div>
